d4fba8c6d234be058700dd8c21aa2c9323576fdc
sandym <91996681+sandyml@users.noreply.github.com>
2022-09-27 14:28:50 -0400

    commit tree e0faae6b5e6692b88691ae5e469a1c17727d1d74

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -12,7 +12,7 @@ function Stock({ stock, onClickHandler }) {
       <div className="card">
         <div className="card-body">
           <h5 className="card-title">{stock.name}</h5>
-          <p className="card-text">$ {stock.price}</p>
+          <p className="card-text">{stock.ticker}: ${stock.price}</p>
         </div>
       </div>
     </div>
